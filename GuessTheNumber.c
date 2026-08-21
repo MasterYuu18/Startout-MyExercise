e2be1e3703ecd27d4dbbeeaa9482e7a3a985990f
MasterYuu18 <mcforever6666@gmail.com>
2026-08-21 22:50:07 +0800

    Add files via upload
    Guess a number between 1 and 100!

---
+++ GuessTheNumber.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(void)
+{
+    srand(time(0));
+    int num = rand()%100+1;
+    int count = 1;
+    int x = 0;
+    printf("请输入一个1到100内的整数：\n");
+    do {
+        scanf("%d", &x);
+        count ++;
+        if (x > num) {
+            printf("你猜大了\n");
+        } else if (x < num) {
+            printf("你猜小了\n");
+        }
+    } while (x != num);
+    printf("你猜对了！总共猜了%d次。\n",count);
+    return 0;
+}
