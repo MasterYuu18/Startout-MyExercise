43b2ee455c0bb842c86f9af56281d381225356cb
MasterYuu18 <mcforever6666@gmail.com>
2026-08-21 11:17:51 +0800

    Adjustments
    I delete redundant "printf"s and add the only one at the bottom.

--- 20260821CExercise.c
+++ 20260821CExercise.c
@@ -6,19 +6,15 @@ int main(void)
     int b = 0;
     if (a / 1000 > 0) {
         b = 4;
-        printf("%d", b);
     } else if (a / 1000 < 0, a / 100 > 0) {
             b = 3;
-            printf("%d", b);
     } else if (a / 100 < 0, a / 10 > 0) {
             b = 2;
-            printf("%d",b);
     } else {
         b = 1;
-        printf("%d", b);
 
     }
-    
+    printf("&d", b);
     return 0;
 
 }
